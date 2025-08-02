b9ed77cd4b170e027496cc1a7d36a0f9f883269c
chaseungjoon <cktmdwns3@gmail.com>
2025-08-02 22:36:38 +0900

    Fix rb file

--- Formula/yt-auto-dl.rb
+++ Formula/yt-auto-dl.rb
@@ -3,10 +3,9 @@ class YtAutoDl < Formula
   homepage "https://github.com/chaseungjoon/yt-auto-dl"
   url "https://github.com/chaseungjoon/yt-auto-dl/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "ee118efa3bfc7a74f134ac9dadecc734c5586df982c8ac8a3e3f98e50a226861"
-  version "1.0.0"
 
-  depends_on "yt-dlp"
   depends_on "ffmpeg"
+  depends_on "yt-dlp"
 
   def caveats
     <<~EOS
@@ -18,6 +17,13 @@ class YtAutoDl < Formula
   def install
     bin.install "dlv"
     bin.install "dla"
+  end
+
+  def post_install
+    <<~EOS
+      For video re-encoding, you must install ffpb:
+        pip install ffpb
+    EOS
   end
 
   test do
